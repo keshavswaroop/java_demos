7ceabba17b097c2a3f135142fe1897692ae4cba3
Swaroop S <swas@BLR.ALLEGISINDIA.COM>
2023-02-23 12:07:35 +0530

    first program

---
+++ javaSampleProject/src/javaSampleProject/firstClass.java
@@ -0,0 +1,8 @@
+package javaSampleProject;
+
+public class firstClass {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ javaSampleProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author swas
+ *
+ */
+module javaSampleProject {
+}
